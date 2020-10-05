2e1799795939e108f2d132c09c8eb98257631f92
enoch-dev <ray@tubelo.com>
2020-10-05 02:01:02 -0500

    add updates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,157 @@
 <template>
-  <button @click="increment">
-    Count is: {{ state.count }}, double is: {{ state.double }}
-  </button>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/batman.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Frame Interpolation
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Upload your video to increase frame rate.
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Movie File Upload
+        </h2>
+<v-btn
+      :loading="loading"
+      :disabled="loading"
+      color="blue-grey"
+      class="ma-2 white--text"
+      @click="loader = 'loading'"
+    >
+      Upload
+      <v-icon
+        right
+        dark
+      >
+        mdi-cloud-upload
+      </v-icon>
+    </v-btn>
+       </v-col>
+
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import { computed, reactive } from '@vue/composition-api'
+  export default {
+    name: 'HelloWorld',
+    data: () => ({
+      loader: null,
+      loading: false,
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+    watch: {
+      loader () {
+        const l = this.loader
+        this[l] = !this[l]
 
-export default {
-  setup() {
-    const state = reactive({
-      count: 0,
-      double: computed(() => state.count * 2)
-    })
+        setTimeout(() => (this[l] = false), 3000)
 
-    function increment() {
-      state.count++
+        this.loader = null
+      },
+    },
+  }
+</script>
+<style scoped>
+.custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
     }
-
-    return {
-      state,
-      increment
+    to {
+      transform: rotate(360deg);
     }
   }
-}
-</script>
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
