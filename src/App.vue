420dd0bdfed1cc4c8f0c53d36697bff45b007ecc
idaf_ai <theo@idaf.ng>
2020-10-14 21:06:02 -0500

    fixed corrupted file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-<<<<<<< HEAD
     <v-app-bar app></v-app-bar>
 
     <v-main>
@@ -11,28 +10,12 @@
 
 <script>
 import FrameInterpolation from './components/FrameInterpolation'
-=======
-  <!-- <v-app-bar app></v-app-bar> -->
-
-  <v-main>
-    <Main/>
-  </v-main>
-</v-app>
-</template>
-
-<script>
-import Main from './components/Main';
->>>>>>> cb408a2ca5e190f9ad402a28ab3f89876a90fdf1
 
 export default {
   name: 'App',
 
   components: {
-<<<<<<< HEAD
-    FrameInterpolation
-=======
-    Main,
->>>>>>> cb408a2ca5e190f9ad402a28ab3f89876a90fdf1
+    FrameInterpolation,
   },
 
   data: () => ({
